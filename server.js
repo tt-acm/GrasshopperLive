5d7f3416d0af39103a8340263e5fa2934ea5646c
Elcin <eertugrul@thorntontomasetti.com>
2019-01-26 12:00:51 -0500

    adding user id message

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-const port= process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
@@ -10,19 +10,20 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
     console.log('a user connected');
+    io.emit('chat message', 'new user connected' + socket.id);
     // socket.on('disconnect', function() {
     //     console.log('user disconnected')
     // });
 
-    socket.on('chat message', function(msg){
-// this send to everyone
-//io.emit('some event', { for: 'everyone' });
+    socket.on('chat message', function(msg) {
+        // this send to everyone
+        //io.emit('some event', { for: 'everyone' });
         io.emit('chat message', msg);
         console.log('message: ' + msg);
-      });
+    });
 
 });
 
 http.listen(port, function() {
-    console.log(`Server running at port `+ port);
+    console.log(`Server running at port ` + port);
 });
