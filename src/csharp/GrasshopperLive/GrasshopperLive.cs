64805fb197030b785520dfdf4f127fff1184d2a5
Emil Poulsen <emil-poulsen@hotmail.com>
2019-01-26 12:04:13 +0100

    adding SocketIO4Net lib and test code

--- src/csharp/GrasshopperLive/GrasshopperLive.cs
+++ src/csharp/GrasshopperLive/GrasshopperLive.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SocketIOClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,6 +25,66 @@ namespace GrasshopperLive
         public void JoinSession()
         {
 
+        }
+
+        public void Test()
+        {
+            var socket = new Client("http://127.0.0.1:3000/"); // url to nodejs 
+            socket.Opened += SocketOpened;
+            socket.Message += SocketMessage;
+            socket.SocketConnectionClosed += SocketConnectionClosed;
+            socket.Error += SocketError;
+
+
+            // register for 'connect' event with io server
+            socket.On("connect", (fn) =>
+            {
+
+                // emit Json Serializable object, anonymous types, or strings
+                string newPart = "some data";
+                //Part newPart = new Part()
+                //{ PartNumber = "K4P2G324EC", Code = "DDR2", Level = 1 };
+
+                socket.Emit("partInfo", newPart);
+            });
+
+            // register for 'update' events - message is a json 'Part' object
+            socket.On("update", (data) =>
+            {
+                //Console.WriteLine("recv [socket].[update] event");
+                //Console.WriteLine("  raw message:      {0}", data.RawMessage);
+                //Console.WriteLine("  string message:   {0}", data.MessageText);
+                //Console.WriteLine("  json data string: {0}", data.Json.ToJsonString());
+                //Console.WriteLine("  json raw:         {0}", data.Json.Args[0]);
+
+                // cast message as Part - use type cast helper
+                //Part part = data.Json.GetFirstArgAs<Part>();
+                //Console.WriteLine(" Part Level:   {0}\r\n", part.Level);
+            });
+
+            // make the socket.io connection
+            socket.Connect();
+
+        }
+
+        private void SocketError(object sender, ErrorEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void SocketConnectionClosed(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void SocketMessage(object sender, MessageEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void SocketOpened(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
         }
     }
 }
