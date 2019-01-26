1d6c2df528382160ecd83c5ec5985e183665f887
Emil Poulsen <emil-poulsen@hotmail.com>
2019-01-26 20:03:40 +0100

    cleanups in core project

---
+++ src/csharp/GrasshopperLive.Core/GhLiveEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrasshopperLive
+{
+    public class GhLiveEventArgs : EventArgs
+    {
+        public GhLiveMessage TheObject { get; set; }
+        //public 
+    }
+}
