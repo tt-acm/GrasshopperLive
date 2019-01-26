741a7e2f2a4d91740b3c5bd0ed63d25337d6639f
Emil Poulsen <emil-poulsen@hotmail.com>
2019-01-26 20:15:12 +0100

    refined schema and added more info

--- src/csharp/GrasshopperLive.Core/GhLiveMessage.cs
+++ src/csharp/GrasshopperLive.Core/GhLiveMessage.cs
@@ -6,9 +6,39 @@ using System.Threading.Tasks;
 
 namespace GrasshopperLive
 {
+    /// <summary>
+    /// Update message which could be either add, delete or move.
+    /// </summary>
     public class GhLiveMessage
     {
+        /// <summary>
+        /// Custom Id defined by us..
+        /// </summary>
+        public Guid CustomId { get; set; }
+
+        /// <summary>
+        /// Corresponds to the CLASS (not the instance!)
+        /// </summary>
+        public Guid ComponentId { get; set; }
+
+        /// <summary>
+        /// Defines: 1. added location or updated location
+        /// </summary>
+        public GhLivePoint Point { get; set; }
+
+        /// <summary>
+        /// Type of update..
+        /// </summary>
+        public UpdateType UpdateType { get; set; }
+
+        /// <summary>
+        /// Sender Id
+        /// </summary>
         public string Sender { get; set; }
+
+        /// <summary>
+        /// Extra message 
+        /// </summary>
         public string Message { get; set; }
     }
 }
