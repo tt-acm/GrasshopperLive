5ae4916a0298746cf53f5a9d3828c4120d00b0b7
Emil Poulsen <emil-poulsen@hotmail.com>
2019-01-26 10:46:01 +0100

    added grasshopperlive class to manage communicaiton

---
+++ src/csharp/GrasshopperLive/GrasshopperLive.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrasshopperLive
+{
+    /// <summary>
+    /// Class to deal with communication. Send/receive messages
+    /// </summary>
+    public class GrasshopperLive
+    {
+        public GrasshopperLive()
+        {
+
+        }
+
+        public void CreateSession()
+        {
+
+        }
+
+        public void JoinSession()
+        {
+
+        }
+    }
+}
