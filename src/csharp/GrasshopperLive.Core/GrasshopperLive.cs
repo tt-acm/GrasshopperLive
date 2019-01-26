3da223bb260e630becc79da71b2b102e380e07a1
Emil Poulsen <emil-poulsen@hotmail.com>
2019-01-26 13:36:02 +0100

    changed socket lib. sending messages to node works

--- src/csharp/GrasshopperLive.Core/GrasshopperLive.cs
+++ src/csharp/GrasshopperLive.Core/GrasshopperLive.cs
@@ -1,4 +1,4 @@
-﻿using SocketIOClient;
+﻿using Quobject.SocketIoClientDotNet.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,75 +30,28 @@ namespace GrasshopperLive
 
         }
 
-        private Client socket;
-
-        public void Test()
+        Socket socket;
+    public void Test()
         {
-            socket = new Client("http://localhost:3000"); // url to nodejs 
-            socket.Opened += SocketOpened;
-            socket.Message += SocketMessage;
-            socket.SocketConnectionClosed += SocketConnectionClosed;
-            socket.Error += SocketError;
+            socket = IO.Socket(ConnectAddress);
 
-
-            // register for 'connect' event with io server
-            //socket.Connect();
-            //return;
-
-            socket.On("connect", (fn) =>
+            socket.On(Socket.EVENT_CONNECT, () =>
             {
 
-                // emit Json Serializable object, anonymous types, or strings
-                //string newPart = "some data";
-                //Part newPart = new Part()
-                //{ PartNumber = "K4P2G324EC", Code = "DDR2", Level = 1 };
-                Console.WriteLine("Connected");
-                //socket.Emit("partInfo", newPart);
+                socket.Emit("chat message" ,"hi from C#");
+                Console.WriteLine("Connected!");
+
             });
 
-            // register for 'update' events - message is a json 'Part' object
-            socket.On("update", (data) =>
+            socket.On("hi", (data) =>
             {
-                Console.WriteLine("Update");
-                //Console.WriteLine("recv [socket].[update] event");
-                //Console.WriteLine("  raw message:      {0}", data.RawMessage);
-                //Console.WriteLine("  string message:   {0}", data.MessageText);
-                //Console.WriteLine("  json data string: {0}", data.Json.ToJsonString());
-                //Console.WriteLine("  json raw:         {0}", data.Json.Args[0]);
-
-                // cast message as Part - use type cast helper
-                //Part part = data.Json.GetFirstArgAs<Part>();
-                //Console.WriteLine(" Part Level:   {0}\r\n", part.Level);
+                Console.WriteLine(data);
+                //socket.Disconnect();
             });
 
-            // make the socket.io connection
-            socket.Connect();
-
-        }
-
-        private void SocketError(object sender, ErrorEventArgs e)
-        {
-            //throw new NotImplementedException();
-            Console.WriteLine(string.Format("{0}: {1}:", nameof(SocketError),
-                e.Message));
-        }
-
-        private void SocketConnectionClosed(object sender, EventArgs e)
-        {
-            //throw new NotImplementedException();
-            Console.WriteLine(nameof(SocketConnectionClosed));
-        }
-
-        private void SocketMessage(object sender, MessageEventArgs e)
-        {
-            Console.WriteLine(nameof(SocketMessage));
-            //throw new NotImplementedException();
-        }
-
-        private void SocketOpened(object sender, EventArgs e)
-        {
-            Console.WriteLine(nameof(SocketOpened));
-            //throw new NotImplementedException();
+            //socket.Connect();           
+            
+            
         }
     }
 }
