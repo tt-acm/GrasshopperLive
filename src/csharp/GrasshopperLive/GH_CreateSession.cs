2bf1bf59c4cfeab960505da743fc83cd0e5f74d7
Emil Poulsen <emil-poulsen@hotmail.com>
2019-01-26 10:04:25 +0100

    setting up GH proj

---
+++ src/csharp/GrasshopperLive/GH_CreateSession.cs
@@ -0,0 +1,41 @@
+﻿using Grasshopper.Kernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrasshopperLive
+{
+    public class GH_CreateSession : GH_Component
+    {
+        public GH_CreateSession() :base("Create Session", "CreateSession", "Create " +
+            "a new GrasshopperLive session.", "Live", "Live")
+        {
+
+        }
+
+        public override Guid ComponentGuid
+        {
+            get
+            {
+                return new Guid("4F96C8EB-4862-454A-A3F8-33ACED66F37D");
+            }
+        }
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+
+        }
+    }
+}
---
+++ src/csharp/GrasshopperLive/GH_JoinSession.cs
@@ -0,0 +1,41 @@
+﻿using Grasshopper.Kernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrasshopperLive
+{
+    public class GH_JoinSession : GH_Component
+    {
+        public GH_JoinSession() :base("Join Session", "JoinSession", "Join " +
+            "an existing GrasshopperLive session.", "Live", "Live")
+        {
+            
+        }
+
+        public override Guid ComponentGuid
+        {
+            get
+            {
+                return new Guid("F1B0B8D3-1F30-4BC2-9108-9115977B9558");
+            }
+        }
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+
+        }
+    }
+}
