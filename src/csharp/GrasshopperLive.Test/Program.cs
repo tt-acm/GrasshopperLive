1182b28849e76dde529c08cdc2aed81ed82107bc
Emil Poulsen <emil-poulsen@hotmail.com>
2019-01-26 12:14:00 +0100

    adding test console app

---
+++ src/csharp/GrasshopperLive.Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrasshopperLive.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GrasshopperLive ghlive = new GrasshopperLive();
+
+            ghlive.Test();
+
+            Console.ReadKey();
+            
+
+        }
+    }
+}
